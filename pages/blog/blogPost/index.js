1c387e3c8852051eb31ee25ffdbb56e4e6165042
Gabriela Papp <gpgabika@gmail.com>
2023-04-06 22:08:26 +0100

    first commit

---
+++ pages/blog/blogPost/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const blogPost = () => {
+  return (
+    <div>Hello From Blog Post Page</div>
+  )
+}
+
+export default blogPost
---
+++ pages/blog/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Blog = () => {
+  return (
+    <div>
+
+      <Link href="blog//blogPost
+        ">Blog Post</Link>
+    </div>
+  )
+}
+
+export default Blog
